d972c00db7a3e3769871f6de94a496a90ab69d81
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-09-23 15:52:23 +0200

    test: adding a test for the README example

--- src/perfpy/cli.py
+++ src/perfpy/cli.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from pathlib import Path
+from typing import TYPE_CHECKING
 
 import typer
 from pydantic import ValidationError
@@ -11,6 +11,9 @@ from rich import pretty
 from perfpy.profiler import profile
 from perfpy.report import report
 from perfpy.schema import Profile, ProfileCommands
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 app = typer.Typer()
 
@@ -42,15 +45,19 @@ FileArg = typer.Argument(
     help="Path to the JSON file defining the profiling",
 )
 
+OutputArg = typer.Option("report.csv", exists=False, help="Name of the output CSV file")
+
 
 @app.command()
-def main(file: Path = FileArg) -> None:
+def main(file: Path = FileArg, output: Path = OutputArg) -> None:
     """Profiles list of commands provided."""
     profile_commands = parse_json(file)
 
+    # Run all provided profiling commands
     profile_measures: list[Profile] = [profile(command) for command in profile_commands.commands]
 
-    report(profile_measures, Path("report.csv"))
+    # Generate report
+    report(profile_measures, output)
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,9 @@
 """Test perfpy CLI."""
 
+import csv
+import json
+from pathlib import Path
+
 from typer.testing import CliRunner
 
 from perfpy.cli import app
@@ -7,9 +11,29 @@ from perfpy.cli import app
 runner = CliRunner()
 
 
-def test_fire() -> None:
-    """Test that the fire command works as expected."""
-    name = "GLaDOS"
-    result = runner.invoke(app, ["--name", name])
+def test_happy_path_example(tmp_path: Path) -> None:
+    """Ensure the example from the README works."""
+    to_profile = tmp_path / "to_profile.json"
+    report = tmp_path / "report.csv"
+
+    with to_profile.open("w", newline="") as fp:
+        json.dump(
+            {
+                "commands": [
+                    {"name": "sysconfig", "command": "python -m sysconfig"},
+                    {"name": "zen_of_python", "command": "python -m this"},
+                ]
+            },
+            fp,
+        )
+
+    result = runner.invoke(app, [str(to_profile), "--output", str(report)])
+
     assert result.exit_code == 0
-    assert name in result.stdout
+    assert report.exists()
+
+    with report.open("r") as fp:
+        csv_reader = csv.DictReader(fp)
+        for line in csv_reader:
+            assert line.get("name") is not None
+            assert line.get("command") is not None
